affb15916ad80bc3c27939f0ea40ff7b70d8bed3
Joey Gibson <joegibso@cisco.com>
2018-02-28 13:50:29 -0700

    Added basic structure

--- tinpi/Voting.sol
+++ tinpi/Voting.sol
@@ -1,40 +1,51 @@
 pragma solidity ^0.4.18; //We have to specify what version of compiler this code will use
 
 contract Voting {
-  /* mapping is equivalent to an associate array or hash
-  The key of the mapping is candidate name stored as type bytes32 and value is
-  an unsigned integer which used to store the vote count
-  */
-  mapping (bytes32 => uint8) public votesReceived;
-  
-  /* Solidity doesn't let you create an array of strings yet. We will use an array of bytes32 instead to store
-  the list of candidates
-  */
-  
-  bytes32[] public candidateList;
-
-  // Initialize all the contestants
-  function Voting(bytes32[] candidateNames) public {
-    candidateList = candidateNames;
-  }
-
-  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
-    require(validCandidate(candidate));
-    return votesReceived[candidate];
-  }
-
-  function voteForCandidate(bytes32 candidate) public {
-    require(validCandidate(candidate));
-    votesReceived[candidate] += 1;
-  }
-
-  function validCandidate(bytes32 candidate) view public returns (bool) {
-    for(uint i = 0; i < candidateList.length; i++) {
-      if (candidateList[i] == candidate) {
-        return true;
-      }
+    struct Topic {
+        uint id;
+        bytes32 name;
+        bytes32 description;
+        address creator;
+        address[] votes;
     }
-    return false;
-  }
+
+    mapping(uint => Topic) public topics;
+
+    function Voting() public {
+    }
+
+    function addTopic(bytes32 name, bytes32 description) public returns (uint8) {
+        // ensure topic doesn't exist
+        // create topic; add to topics; return id
+    }
+
+    function removeTopic(uint id) returns (bool) {
+        // ensure topic exists
+        // remove topic from topics; return true on success
+    }
+
+    function voteForTopic(uint id) public returns (bool) {
+        // ensure topic exists
+        // ensure address of caller hasn't already voted for this topic
+        // add caller's address to the `votes` field of the topic
+        // return true on success
+    }
+
+    function rescindVoteForTopic(uint id) public returns (bool) {
+        // ensure topic exists
+        // ensure address of caller has already voted for this topic
+        // remove caller's address from the `votes` field of the topic
+        // return true on success
+    }
+
+    function totalVotesForTopic(uint id) view public returns (uint8) {
+        // ensure topic exists
+        // return count of address in the `votes` field
+    }
+
+    function getAllTopicsAndVoteCounts() view public returns (mapping(uint => uint8)) {
+        // sort topics by length of `votes` field
+        // return mapping of topic `id` to length of `votes` field
+    }
 }
 

3125c52b11e16e3f4c600b58dfe8d3a1284ffcc8
Joey Gibson <joegibso@cisco.com>
2018-02-28 14:54:27 -0700

    Added addTopic and getAll

--- tinpi/Voting.sol
+++ tinpi/Voting.sol
@@ -9,14 +9,36 @@ contract Voting {
         address[] votes;
     }
 
-    mapping(uint => Topic) public topics;
+    Topic[] public topics;
+    uint lastId = 0;
 
     function Voting() public {
     }
 
     function addTopic(bytes32 name, bytes32 description) public returns (uint) {
         // ensure topic doesn't exist
-        // create topic; add to topics; return id
+        for (uint i = 0; i < topics.length; i++) {
+            Topic memory t = topics[i];
+
+            if (t.name == name && t.description == description) {
+                return 0;
+            }
+        }
+
+        // create topic; add to topics
+        address creator = msg.sender;
+
+        Topic memory topic = Topic({
+            id : generateTopicId(),
+            name : name,
+            description : description,
+            creator : creator,
+            votes : new address[](0)
+            });
+
+        topics.push(topic);
+
+        return topic.id;
     }
 
     function removeTopic(uint id) public returns (bool) {
@@ -38,9 +60,28 @@ contract Voting {
         // return true on success
     }
 
-    function getAllTopicsAndVoteCounts() view public returns (Topic[]) {
+    function getAllTopicsAndVoteCounts() view public returns (uint[], bytes32[], bytes32[], address, uint[]) {
         // sort topics by length of `votes` field
         // return list of topics
+        uint[] memory ids = new uint[](topics.length);
+        bytes32[] memory names = new bytes32[](topics.length);
+        bytes32[] memory descriptions = new bytes32[](topics.length);
+        uint[] memory voteCounts = new uint[](topics.length);
+
+        for (uint i = 0; i < topics.length; i++) {
+            Topic storage topic = topics[i];
+            ids[i] = topic.id;
+            names[i] = topic.name;
+            descriptions[i] = topic.description;
+            voteCounts[i] = topic.votes.length;
+        }
+
+        return (ids, names, descriptions, topic.creator, voteCounts);
+    }
+
+    function generateTopicId() private returns (uint newId) {
+        newId = lastId + 1;
+        lastId = newId;
     }
 }
 

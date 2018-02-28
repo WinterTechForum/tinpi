95cb49965462266977128552d8a85fb1e4efa3b3
Octavian Geagla <ogeagla@gmail.com>
2018-02-28 15:54:54 -0700

    [test] wip addTopic test - failing (out of gas)

--- tinpi/test/Voting.test.js
+++ tinpi/test/Voting.test.js
@@ -3,7 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
-const {interface, bytecode } = require('../compile');
+const {interface, bytecode} = require('../compile');
 
 let accounts;
 let voting;
@@ -14,14 +14,27 @@ beforeEach(async () => {
 
     // Use the first account found for tests
     voting = await new web3.eth.Contract(JSON.parse(interface))
-                .deploy({ data: bytecode, arguments: [] })
-                .send({ from: accounts[0], gas: '1000000' })
-    
+        .deploy({data: bytecode, arguments: []})
+        .send({from: accounts[0], gas: 1000000, gasPrice: '5'});
+
     voting.setProvider(provider);
 });
 
-describe('Voting', () => {
+describe('Voting - has addr', () => {
     it('deploys a contract', () => {
         assert.ok(voting.options.address);
+    });
+});
+
+
+describe('Voting - calls addTopic', () => {
+    it('deploys a contract', () => {
+        console.log(voting)
+        assert
+            .ok(voting
+                .methods
+                .addTopic(web3.utils.asciiToHex("name"),
+                    web3.utils.asciiToHex("desc and stuff"))
+                .call({from: web3.eth.accounts[0]}));
     });
 });
